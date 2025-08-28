98432ef6f99674c21ba95281f26661fbab2fe231
Òscar Muntal <oscar.muntal@gmail.com>
2025-08-28 15:24:55 +0200

    ExchangeInfo screen basic UI

---
+++ LOTRConverter/ExchangeInfo.swift
@@ -0,0 +1,18 @@
+//
+//  ExchangeInfo.swift
+//  LOTRConverter
+//
+//  Created by Òscar Muntal on 28/8/25.
+//
+
+import SwiftUI
+
+struct ExchangeInfo: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    ExchangeInfo()
+}
