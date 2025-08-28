1700d7e633292bfe07b664521d7db9676b4b7171
Òscar Muntal <oscar.muntal@gmail.com>
2025-08-28 11:30:12 +0200

    Initial Commit

---
+++ LOTRConverter/LOTRConverterApp.swift
@@ -0,0 +1,17 @@
+//
+//  LOTRConverterApp.swift
+//  LOTRConverter
+//
+//  Created by Òscar Muntal on 28/8/25.
+//
+
+import SwiftUI
+
+@main
+struct LOTRConverterApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
