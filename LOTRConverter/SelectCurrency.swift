dfaddf0cc9edfffd1617a930a004cb52a6086619
Òscar Muntal <oscar.muntal@gmail.com>
2025-09-01 19:27:14 +0200

    Basic selectCurrency screen

---
+++ LOTRConverter/SelectCurrency.swift
@@ -0,0 +1,72 @@
+//
+//  SelectCurrency.swift
+//  LOTRConverter
+//
+//  Created by Òscar Muntal on 28/8/25.
+//
+
+import SwiftUI
+
+struct SelectCurrency: View {
+    @Environment(\.dismiss) var dismiss
+    
+    var body: some View {
+        ZStack {
+            // Background
+            Image(.parchment)
+                .resizable()
+                .ignoresSafeArea()
+                .background(.brown)
+            
+            VStack {
+                // Text
+                Text("Select the currency you are starting with:")
+                    .fontWeight(.bold)
+                
+                // Currency icons
+                ZStack(alignment: .bottom) {
+                    Image(.copperpenny)
+                        .resizable()
+                        .scaledToFit()
+                    
+                    Text("Cooper Penny")
+                        .padding(3)
+                        .font(.caption)
+                        .frame(maxWidth: .infinity)
+                        .background(.brown.opacity(0.75))
+                }
+                .padding(3)
+                .frame(width: 100, height: 100)
+                .background(.brown)
+                .clipShape(.rect(cornerRadius: 25))
+                
+                
+                // Text
+                Text("Select the currency you would like to convert to:")
+                    .fontWeight(.bold)
+                
+                // Currency icons
+                
+                // Done button
+                Button("Done") {
+                    dismiss()
+                }
+                .buttonStyle(.borderedProminent)
+                .tint(.brown.mix(with: .black, by: 0.2))
+                .font(.largeTitle)
+                .padding()
+                .foregroundStyle(.white)
+            }
+            .padding()
+            .multilineTextAlignment(.center)
+        }
+    }
+}
+
+#Preview {
+    VStack {
+        SelectCurrency()
+    }
+}
+
+
