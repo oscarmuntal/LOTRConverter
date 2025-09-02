9e25490f16a08f91f37343f7392c0973bf46fe81
Òscar Muntal <oscar.muntal@gmail.com>
2025-09-02 12:11:54 +0200

    IconGrid View

---
+++ LOTRConverter/IconGrid.swift
@@ -0,0 +1,41 @@
+//
+//  IconGrid.swift
+//  LOTRConverter
+//
+//  Created by Òscar Muntal on 2/9/25.
+//
+
+import SwiftUI
+
+struct IconGrid: View {
+    @State var selectedCurrency: Currency
+    
+    var body: some View {
+        LazyVGrid(columns: [GridItem(), GridItem(), GridItem()]) {
+            ForEach(Currency.allCases) { currency in
+                if selectedCurrency == currency {
+                    CurrencyIcon(currencyImage: currency.image, currencyName: currency.name)
+                        .shadow(color: .black, radius: 10)
+                        .overlay {
+                            RoundedRectangle(cornerRadius: 25)
+                                .stroke(lineWidth: 3)
+                                .opacity(0.5)
+                        }
+                } else {
+                    CurrencyIcon(currencyImage: currency.image, currencyName: currency.name)
+                        .onTapGesture {
+                            selectedCurrency = currency
+                        }
+                }
+            }
+        }
+    }
+}
+
+#Preview {
+    VStack {
+        IconGrid(selectedCurrency: .goldPenny)
+    }
+}
+
+
